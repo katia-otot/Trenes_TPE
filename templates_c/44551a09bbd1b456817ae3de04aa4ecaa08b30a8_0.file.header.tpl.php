e97bc95947148c2914aca69a2366113ead536388
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2023-05-30 18:34:37 -0300

    agrego templates c

--- templates_c/44551a09bbd1b456817ae3de04aa4ecaa08b30a8_0.file.header.tpl.php
+++ templates_c/44551a09bbd1b456817ae3de04aa4ecaa08b30a8_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-05-30 03:07:46
+/* Smarty version 4.3.1, created on 2023-05-30 22:24:31
   from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_64754c623b3bc0_72262968',
+  'unifunc' => 'content_64765b7fc880d6_44591763',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '44551a09bbd1b456817ae3de04aa4ecaa08b30a8' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\header.tpl',
-      1 => 1685404316,
+      1 => 1685478269,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_64754c623b3bc0_72262968 (Smarty_Internal_Template $_smarty_tpl) {
+function content_64765b7fc880d6_44591763 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,8 +28,8 @@ function content_64754c623b3bc0_72262968 (Smarty_Internal_Template $_smarty_tpl)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<?php echo '<script'; ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <?php echo '<script'; ?>
  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"><?php echo '</script'; ?>
 >
     
--- templates_c/f670b8f2dcc3b0f1717626b8f8045190bfd1d4b8_0.file.login.tpl.php
+++ templates_c/f670b8f2dcc3b0f1717626b8f8045190bfd1d4b8_0.file.login.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-05-30 04:28:51
+/* Smarty version 4.3.1, created on 2023-05-30 22:24:51
   from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\login.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_64755f633a2371_32677523',
+  'unifunc' => 'content_64765b93ba4457_21948168',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f670b8f2dcc3b0f1717626b8f8045190bfd1d4b8' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\login.tpl',
-      1 => 1685413729,
+      1 => 1685478269,
       2 => 'file',
     ),
   ),
@@ -22,10 +22,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_64755f633a2371_32677523 (Smarty_Internal_Template $_smarty_tpl) {
+function content_64765b93ba4457_21948168 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-  <div class="container">
+<div class="container">
   <div class="row">
     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
       <div class="card border-0 shadow rounded-3 my-5">
