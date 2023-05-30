012acbb4df67b716192991d3d382c7b73a7f3105
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2023-05-30 20:53:12 -0300

    vagones tpl

--- templates_c/12918525d5ce604f1b8b03cefd24e5042fcdbc6b_0.file.vagones.tpl.php
+++ templates_c/12918525d5ce604f1b8b03cefd24e5042fcdbc6b_0.file.vagones.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-05-31 00:24:01
+/* Smarty version 4.3.1, created on 2023-05-31 00:28:41
   from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\vagones.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_64767781c74965_81777421',
+  'unifunc' => 'content_64767899b090b6_27025211',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '12918525d5ce604f1b8b03cefd24e5042fcdbc6b' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\vagones.tpl',
-      1 => 1685485436,
+      1 => 1685485720,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_64767781c74965_81777421 (Smarty_Internal_Template $_smarty_tpl) {
+function content_64767899b090b6_27025211 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:nav.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -62,7 +62,7 @@ $_smarty_tpl->tpl_vars['vagon']->do_else = false;
 </td>
                 <td><?php echo $_smarty_tpl->tpl_vars['vagon']->value->locomotora_modelo;?>
 </td>
-                <td><input class="btn btn-outline-secondary" type="submit" value="Ver más" /></td>
+                <td><input class="btn btn-outline-secondary" type="submit" value="Ver más" ></td>
             </tr>
         <?php
 }
