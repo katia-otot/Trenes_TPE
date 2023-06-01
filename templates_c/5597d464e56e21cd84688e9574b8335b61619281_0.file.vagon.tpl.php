e2f96dc75aa58bfa0ba8ee1e95be9b67ea848835
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2023-06-01 20:21:26 -0300

    templates c

---
+++ templates_c/5597d464e56e21cd84688e9574b8335b61619281_0.file.vagon.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-06-02 01:20:54
+  from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\vagon.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_647927d64ef035_31696883',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5597d464e56e21cd84688e9574b8335b61619281' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\vagon.tpl',
+      1 => 1685661587,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:header.tpl' => 1,
+    'file:nav.tpl' => 1,
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_647927d64ef035_31696883 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender("file:nav.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+
+
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
