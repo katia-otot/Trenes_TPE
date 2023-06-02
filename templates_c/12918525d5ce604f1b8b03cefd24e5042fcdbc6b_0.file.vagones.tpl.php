0dcccf8ff87d81df80c9640eb3ec2a0d30c48c7a
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2023-06-02 20:56:13 -0300

    teamplates c

--- templates_c/12918525d5ce604f1b8b03cefd24e5042fcdbc6b_0.file.vagones.tpl.php
+++ templates_c/12918525d5ce604f1b8b03cefd24e5042fcdbc6b_0.file.vagones.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-06-03 01:52:45
+/* Smarty version 4.3.1, created on 2023-06-03 01:55:49
   from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\vagones.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_647a80cd398be0_50851130',
+  'unifunc' => 'content_647a8185d70469_58896243',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '12918525d5ce604f1b8b03cefd24e5042fcdbc6b' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\vagones.tpl',
-      1 => 1685749959,
+      1 => 1685750134,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_647a80cd398be0_50851130 (Smarty_Internal_Template $_smarty_tpl) {
+function content_647a8185d70469_58896243 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender("file:nav.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
@@ -73,6 +73,43 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     </tbody>
     </table>
 </section>
+<section>
+        <form method="POST">
+    <div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Locomotora</span>
+
+    <select class="form-select" aria-label="Default select example">
+  <option selected>Poner locomotoras</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+</div>
+    <div class="input-group input-group-sm mb-3">
+  <span class="input-group-text " id="inputGroup-sizing-sm">Número de vagón</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+    <div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Tipo de vagón</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+<div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Capacidad máxima</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+<div class="input-group input-group-sm mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-sm">Modelo</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+</div>
+<div class="d-grid">
+<div style="display:flex; justify-content: center">
+
+<button class="btn   text-uppercase fw-bold btn-outline-secondary" style=" width:80px;
+height:45px; margin-bottom:1%; align:center"type="submit">Enviar</button>
+</div>
+</div>
+
+    </form>
 <?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
 }
