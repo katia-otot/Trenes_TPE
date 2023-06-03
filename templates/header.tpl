6e6a345e7018305fe8b3cd7d3d93bb0b33f21444
Luna Rodriguez <luna.candela.rodriguez@gmail.com>
2023-06-03 17:42:36 -0300

    agregue imagen en header

--- templates/header.tpl
+++ templates/header.tpl
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/66/66591.png?w=740&t=st=1685824774~exp=1685825374~hmac=7296ccc4cd65ee87c767476a425a64e0ea534d3b2f2c257b53d072346228005a">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     
--- templates_c/44551a09bbd1b456817ae3de04aa4ecaa08b30a8_0.file.header.tpl.php
+++ templates_c/44551a09bbd1b456817ae3de04aa4ecaa08b30a8_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-06-03 06:25:11
+/* Smarty version 4.3.1, created on 2023-06-03 22:39:43
   from 'C:\xampp\htdocs\WEB_TPE\Trenes_TPE\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_647ac0a7d96c53_60956180',
+  'unifunc' => 'content_647ba50f1db397_38501413',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '44551a09bbd1b456817ae3de04aa4ecaa08b30a8' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB_TPE\\Trenes_TPE\\templates\\header.tpl',
-      1 => 1685484108,
+      1 => 1685824779,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_647ac0a7d96c53_60956180 (Smarty_Internal_Template $_smarty_tpl) {
+function content_647ba50f1db397_38501413 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,6 +28,7 @@ function content_647ac0a7d96c53_60956180 (Smarty_Internal_Template $_smarty_tpl)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/66/66591.png?w=740&t=st=1685824774~exp=1685825374~hmac=7296ccc4cd65ee87c767476a425a64e0ea534d3b2f2c257b53d072346228005a">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <?php echo '<script'; ?>
  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"><?php echo '</script'; ?>
