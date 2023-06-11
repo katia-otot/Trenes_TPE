4ac60a79d2417f4f6b7eb17b3aae2346f4224787
Katia Gadea <katiagadea19@gmail.com>
2023-06-11 13:48:53 -0300

    Falta seguir corregiendo cosas

--- models/vagonesModel.php
+++ models/vagonesModel.php
@@ -15,7 +15,7 @@ class vagonesModel{
     }
     
     public function getVagonesDeLocomotora ($locomotora_id){
-        $sentencia = $this -> db -> prepare("SELECT vagon.* , locomotora.modelo as locomotora_modelo FROM vagon JOIN locomotora ON vagon.locomotora_id = locomotora.id_locomotora and locomotora_id = ?");
+        $sentencia = $this -> db -> prepare("SELECT vagon.* , locomotora.modelo as locomotora_modelo FROM vagon JOIN locomotora ON vagon.locomotora_id = locomotora.id_locomotora WHERE locomotora_id = ?");
         $sentencia -> execute([$locomotora_id]);
         $vagon = $sentencia -> fetchAll(PDO::FETCH_OBJ);
         return $vagon;
